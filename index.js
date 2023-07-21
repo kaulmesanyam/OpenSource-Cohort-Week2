1efb1374e3062f52e07a8905a1d1b2f457916d17
kaulmesanyam <kaulsanyam@gmail.com>
2023-07-21 15:26:57 +0530

    implemented route to take counter via body

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 app.use(middleware1);
+app.use(bodyParser.json());
 
 function middleware1(req, res, next) {
-    console.log(req.headers.counter);
+    console.log(`From inside the middleware: ${req.headers.counter}`);
     next();
 }
 
@@ -18,7 +20,14 @@ app.get('/sendCounterInQuery', (req, res) => {
 
 app.post('/sendCounterInHeader', (req, res) => {
     let calculate = req.headers.counter;
-    console.log(req.headers);
+    let calculatedSum = calculateSum(+calculate);
+    let response = `The sum is: ${calculatedSum}`;
+    res.send(response);
+});
+
+app.post('/sendCounterInBody', (req, res) => {
+    console.log(req.body);
+    let calculate = req.body.counter;
     let calculatedSum = calculateSum(+calculate);
     let response = `The sum is: ${calculatedSum}`;
     res.send(response);
