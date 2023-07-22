9df9015946f5acdc7f3d67e3095b593da2b20529
kaulmesanyam <kaulsanyam@gmail.com>
2023-07-22 17:55:29 +0530

    made changes to use __dirname as its not available in ES Module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from  'url'
+import { dirname } from 'path';
+
 const app = express()
 const port = 3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(bodyParser.json());
 app.use(middleware1);
